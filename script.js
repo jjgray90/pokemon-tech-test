95a3f8d0e82e4094c523742f7e17fc48207ba136
Your Name <jjgray90@gmail.com>
2022-07-06 15:01:02 +0100

    grabbed types from pokemon array

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import pokemonArray from "./data/pokemon.js";
 
 const cardContainer = document.querySelector(".card-container");
-
-console.log(cardContainer);
+const typeFilter = document.querySelector("#type");
+let capitalizeName = console.log(cardContainer);
 
 const getPokemon = () => {
   pokemonArray.map((pokemon) => {
@@ -10,14 +10,34 @@ const getPokemon = () => {
         <div class="card">
             <img src="${pokemon.sprite}">
             <div class="card__content">
-                <h2 class="card__heading">${pokemon.name}</h2>
-                <p class="card__text">${pokemon.name} (#${
-      pokemon.id
-    }) is a ${pokemon.types.join(" & ")} type pokemon.</p>
+                <h2 class="card__heading">${
+                  pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
+                }</h2>
+                <p class="card__text">${
+                  pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
+                } (#${pokemon.id}) is a ${pokemon.types.join(
+      " & "
+    )} type pokemon.</p>
             </div>
         </div>
         `;
   });
 };
 
-getPokemon();
+const getTypes = () => {
+  const allTypes = [];
+  pokemonArray.forEach((pokemon) =>
+    pokemon.types.forEach((type) => allTypes.push(type))
+  );
+  const pokeTypes = [...new Set(allTypes)].sort();
+  pokeTypes.forEach((type) => {
+    typeFilter.innerHTML += `<option value=${type}>${type}</option>`;
+  });
+
+  console.log(pokeTypes);
+};
+
+getTypes();
+
+
+window.addEventListener("load", getPokemon);
